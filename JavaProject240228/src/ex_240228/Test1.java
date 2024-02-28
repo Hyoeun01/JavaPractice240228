f754c510f2bed9eb0aee6c9b639c6fd066271ebd
Hyoeun <shyoeun09@gmail.com>
2024-02-28 10:30:11 +0900

    first commit

---
+++ JavaProject240228/src/ex_240228/Test1.java
@@ -0,0 +1,17 @@
+package ex_240228;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// vscode처럼 UI 폰트, 글자크기, 자동완성 등 세팅할 수 있음
+		// 자바스크립트는 함수이름을 사용 >> 자바는 이름 : 메서드로 사용
+		// 정의, 호출(=사용)
+		// 역할 = 콘솔에 메세지 출력 . 중요! >> 프로그래밍에서 값이 잘 입출력되는지 반드시 확인해야 함
+		// 실행 > 
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
