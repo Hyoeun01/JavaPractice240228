71cc7ca3de0cc4bebab08345b2ae2eb046efba2a
Hyoeun <shyoeun09@gmail.com>
2024-02-29 11:53:35 +0900

    240229 3rd fin

--- JavaProject240228/src/ex_240229/Ex_3_for.java
+++ JavaProject240228/src/ex_240229/Ex_3_for.java
@@ -5,27 +5,50 @@ import java.util.Scanner;
 public class Ex_3_for {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("자연수를 입력하세요 : ");
-		int number =scanner.nextInt();
-		int result = sumN(number);
-		System.out.println("입력한 수 까지의 합은 "+result+"입니다.");
-	}
-	
-	//1부터 입력한 수까지 모두 더하는 프로그램 만들기
-	
-	public static int sumN(int number) {
-		
-		//초깃값, 합의 값 할당
-		int i, sum = 0;
-		
-		for (i=0 ; i<=number ; i++) {
-			sum+=i ;
+		// while은 계속 실행하도록 하고, 특정 조건에서 빠져나올때 사용한다.
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println(" 입력한 수까지 더하는 프로그램 ");
+			System.out.println(" 자연수를 입력하세요 , 0입력시 종료 ");
+			
+			int number = scanner.nextInt();
+			
+			
+			//조건문 분기
+			if (number >= 1 ) {
+				int result = sumN(number);
+				System.out.println("입력한 수 까지의 합은 " + result + "입니다.");
+			}
+			else if(number == 0) {
+				System.out.println("종료합니다.");
+				
+				//사용안하는 메모리는 꼭 반납할것.
+				scanner.close();
+				break;
+			}
+			else {
+				System.out.println("양수를 입력하세요");
+			}
+			
+
+			
 		}
-		
+
+	}
+
+	// 1부터 입력한 수까지 모두 더하는 프로그램 만들기
+
+	public static int sumN(int number) {
+
+		// 초깃값, 합의 값 할당
+		int i, sum = 0;
+
+		for (i = 0; i <= number; i++) {
+			sum += i;
+		}
+
 		return sum;
-		
+
 	}
 
 }
---
+++ JavaProject240228/src/ex_240229/Ex_4_nested_For.java
@@ -0,0 +1,66 @@
+package ex_240229;
+
+import java.util.Scanner;
+
+public class Ex_4_nested_For {
+
+	public static void main(String[] args) {
+		
+
+		//입력받은 수 까지의 구구단 출력하기. (1~9중 숫자 입력하나 정하기)
+		
+		
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println(" 입력한 수 까지의 구구단 출력하기 ");
+			System.out.println(" 숫자를 입력하세요 , 0입력시 종료 ");
+			
+			int number = scanner.nextInt();
+			
+			
+			//조건문 분기
+			if (number >= 1 && number <=9) {
+				//입력 받은 수로 구구단 출력하기
+				gugudan(number);
+			}
+			else if(number == 0) {
+				System.out.println("프로그램을 종료합니다.");
+				
+				//사용안하는 메모리는 꼭 반납할것.
+				scanner.close();
+				break;
+			}
+			else {
+				System.out.println("0에서 9사이의 값을 입력하세요");
+			}
+			
+
+			
+		}
+
+	}
+
+	// 입력한 수 까지의 구구단 출력하기
+	// 중첩for문 활용
+	
+	public static void gugudan(int number) {
+		
+		for (int i=1 ; i <= number ; i++) {
+			for(int j=1 ; j < 10 ; j++) {
+				System.out.print(i + "*" + j+"="+i*j);
+				System.out.print('\t');
+			}
+			
+			System.out.println(); // 한단 끝나면 줄 넘김
+		}
+
+		
+		
+	}
+
+		
+
+	}
+
+	
+
