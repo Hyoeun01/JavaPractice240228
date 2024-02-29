0b6fde5eafc84ab4c5ce76ea8f7b3d2d37ee4e2c
Hyoeun <shyoeun09@gmail.com>
2024-02-29 09:56:52 +0900

    240229 1교시 선생님꺼 받아서 저장

--- JavaProject240228/src/ex_240229/Ex_1_if_else_if_switch.java
+++ JavaProject240228/src/ex_240229/Ex_1_if_else_if_switch.java
@@ -2,45 +2,44 @@ package ex_240229;
 
 import java.security.PublicKey;
 import java.util.Scanner;
+import java.util.function.BiConsumer;
 
 public class Ex_1_if_else_if_switch {
 
 	public static void main(String[] args) {
-
-		// 성적을 입력해서 , 총점과 평균을 구하는 함수
-		// HTML, CSS, JAVASCRIPT, JAVA 과목에 대한 점수를 입력받아서 반환하기
-		
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("HTML, CSS, JAVASCRIPT, JAVA 과목 점수 입력하기 (예: 80 90 70 60) : ");
+		System.out.println("HTML,CSS,JS,JAVA 과목 점수 입력하기(예: 100 70 80 90 )");
 		int htmlScore = scanner.nextInt();
 		int cssScore = scanner.nextInt();
-		int javascriptScore = scanner.nextInt();
+		int jsScore = scanner.nextInt();
 		int javaScore = scanner.nextInt();
 		
-	    String resultString=toAvgTotalScore(htmlScore,cssScore,javascriptScore,javaScore);
-	    int resultAvgScore = toAvgScore(htmlScore, cssScore, javascriptScore, javaScore);
-	    System.out.println(result);
-	    
-	    if(resultAvgScore>=90) {
-	    	System.out.println("A등급");
-	    } else if (resultAvgScore>=80 && resultAvgScore<90){
-	    	System.out.println("B등급");
-	    	
-	    }
-	    
+		String result = toAvgTotalScore(htmlScore,cssScore,jsScore,javaScore);
+		int resultAvgScore = toAvgScore(htmlScore, cssScore, jsScore, javaScore);
+		
+		System.out.println(result);
+		
+		if(resultAvgScore >= 90 ) {
+			System.out.println(" A 등급입니다");
+		} else if (resultAvgScore >= 80 && resultAvgScore < 90) {
+			System.out.println(" B 등급입니다");
+		}
+		
 	}
 
-	public static String toAvgTotalScore(int html, int css, int javascript, int java) {
-		int resultTotalScore = html + css + javascript + java;
-		int resultTotalAvg = resultTotalScore / 4;
-		String result =	"결과 점수, 총점 : "+resultTotalScore+" 평균 : "+resultTotalAvg+"점 입니다."	;
-		return result ;
+	// 성적을 입력해서, 총점, 평균을 구하는 간단한 함수
+	// HTML, CSS, JS, JAVA , 4과목에 대한 점수를 입력 받아서, 반환하기.
+	public static String toAvgTotalScore(int html, int css, int js, int java) {
+		int resultTotalScore = html + css + js + java;
+		int resultAvgScore = resultTotalScore / 4;
+		String result = "결과 점수 , 총점 : " + resultTotalScore + "점, 평균 : " + resultAvgScore + " 점";
+		return result;
 	}
 	
-	public static int toAvgScore(int html, int css, int javascript, int java) {
-		int resultTotalScore = html + css + javascript + java;
-		int resultTotalAvg = resultTotalScore / 4;
-		return result ;
+	public static int toAvgScore(int html, int css, int js, int java) {
+		int resultTotalScore = html + css + js + java;
+		int resultAvgScore = resultTotalScore / 4;
+		return resultAvgScore;
 	}
 
 }
