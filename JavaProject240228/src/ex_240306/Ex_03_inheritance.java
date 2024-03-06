8cfe40ff37b50dfaa03cf93749e46dd354f04bb8
Hyoeun <shyoeun09@gmail.com>
2024-03-06 11:49:48 +0900

    240306 3rd fin

--- JavaProject240228/src/ex_240306/Ex_03_inheritance.java
+++ JavaProject240228/src/ex_240306/Ex_03_inheritance.java
@@ -3,19 +3,34 @@ package ex_240306;
 // 부모 클래스 와 클래스 정의
 
 class A2 {
-	public A2() {
+	// 모든 클래스는 묵시적으로 Object 클래스를 상속받고있다.
+	public A2(int x) {
 		System.out.println("생성자 A2입니다");
+	}
+	
+	// 두번째 방법: 디폴트 생성자를 만드는 방법
+	public A2() {
+		
 	}
 }
 
 class B extends A2 {
-	public B() {
+	
+	public B(int x) {
+		// 첫번째 방법 : 부모클래스의 생성자에 타입을 맞춘다  = 깔맞춤
+		super(x);
+		x= 0;
+		
 		System.out.println("생성자 B입니다");
+	}
+	public B() {
+		
 	}
 }
 
 class C extends B {
 	public C() {
+		super(); // 생략이 되어있고, 묵시적이다
 		System.out.println("생성자 C입니다");
 	}
 }
